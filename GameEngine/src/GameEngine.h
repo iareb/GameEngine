e0fbb2bca8ee62c756c52a431061052b96d80414
iareb <lolluz6@gmail.com>
2025-02-13 16:32:45 +0100

    Configurazione iniziale del progetto e dell'entry point
    Definite i primi file e librerie di configurazione.

---
+++ GameEngine/src/GameEngine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Hazel applications
+
+#include "GameEngine/Application.h"
+
+// ---------- Entry Point ----------
+#include "GameEngine/EntryPoint.h"
+// ----------------------------------------

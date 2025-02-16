1f0ab400a683979b8ddf7be8dfb5ce39337f953b
iareb <lolluz6@gmail.com>
2025-02-16 16:52:30 +0100

    Configurazione di Premake come build system e rigenerazione della soluzione
    Eseguire il file GenerateProjects.bat con un doppio click farà rigenerare la soluzione

---
+++ premake5.lua
@@ -0,0 +1,117 @@
+workspace "GameEngine"
+    architecture "x64"
+    startproject "Sandbox"
+
+    configurations 
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+-- ad es.: DEBUG-WINDOWS-X64
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GameEngine"
+    location "GameEngine"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    -- I filtri sono utili per configurazioni che si applicano a una specifica piattaforma
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines 
+        {
+            "HZ_PLATFORM_WINDOWS",
+            "HZ_BUILD_DLL",
+        }
+
+        postbuildcommands
+        {
+            -- Va nella cartella in cui si trova GameEngine.dll e lo copia nella cartella Sandbox
+            ("{COPYFILE} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+    
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On"
+
+    filter "action:vs2022"
+        buildoptions { "/utf-8" }
+
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "GameEngine/vendor/spdlog/include",
+        "GameEngine/src"
+    }
+
+    links
+    {
+        "GameEngine"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines 
+        {
+            "HZ_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On"
+
+    filter "action:vs2022"
+        buildoptions { "/utf-8" }

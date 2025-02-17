e27b4f9fe954f30596b8e86d5f9feed7eeb0b4b2
iareb <lolluz6@gmail.com>
2025-02-17 18:00:32 +0100

    Implementati i callback degli eventi
    Gli eventi: movimento del mouse, click e rilascio di un bottone del mouse, click di un tasto della tastiera, chiusura della finestra

--- GameEngine/src/GameEngine/Application.cpp
+++ GameEngine/src/GameEngine/Application.cpp
@@ -1,21 +1,40 @@
 #include "hzpch.h"
 #include "Application.h"
 
-#include "GameEngine/Events/ApplicationEvent.h"
 #include "GameEngine/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace GameEngine {
 
+// Creiamo un oggetto callable che, quando invocato, eseguirà OnEvent sull'oggetto this (l'istanza di Application) 
+// e gli passerà il primo argomento (std::placeholders::_1) al posto del segnaposto.
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application() 
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		// Se l'evento e è di tipo WindowCloseEvent, il dispatcher chiamerà OnWindowClose()
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		HZ_CORE_TRACE("{0}", e.ToString());
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 	void Application::Run()
--- GameEngine/src/Platform/Windows/WindowsWindow.cpp
+++ GameEngine/src/Platform/Windows/WindowsWindow.cpp
@@ -1,9 +1,18 @@
 #include "hzpch.h"
 #include "WindowsWindow.h"
 
+#include "GameEngine/Events/KeyEvent.h"
+#include "GameEngine/Events/MouseEvent.h"
+#include "GameEngine/Events/ApplicationEvent.h"
+
 namespace GameEngine {
 
 	static bool s_GLFWInitialized = false;
+
+	static void GLFWErrorCallback(int error, const char* description)
+	{
+		HZ_CORE_ERROR("GLFW Error ({0}): {1}", error, description);
+	}
 
 	Window* Window::Create(const WindowProps& props)
 	{
@@ -32,7 +41,7 @@ namespace GameEngine {
 		{
 			int success = glfwInit();
 			HZ_CORE_ASSERTS(success, "Could not initialize GLFW!");
-
+			glfwSetErrorCallback(GLFWErrorCallback);
 			s_GLFWInitialized = true;
 		}
 
@@ -40,6 +49,97 @@ namespace GameEngine {
 		glfwMakeContextCurrent(m_Window);
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
+
+		// Settiamo i callback di GLFW
+
+		// Questa funzione di GLFW consente di registrare una funzione (o lambda) che verrà chiamata ogni volta che la finestra cambia dimensione.
+		// Il secondo parametro è una lambda che accetta tre parametri: 
+		// GLFWwindow* window: puntatore alla finestra in cui si è verificato il cambiamento.
+		// int width e int height : le nuove dimensioni della finestra
+		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height)
+		{
+			// Questa funzione recupera un puntatore a dati utente associati alla finestra. 
+			// Spesso viene usata per memorizzare informazioni personalizzate o per collegare la finestra a un oggetto di gestione.
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			
+			data.Width = width;
+			data.Height = height;
+
+			WindowResizeEvent event(width, height);
+			data.EventCallback(event);
+		});
+
+		glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			WindowCloseEvent event;
+			data.EventCallback(event);
+		});
+
+		glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			switch (action)
+			{
+				case GLFW_PRESS:
+				{
+					KeyPressedEvent event(key, 0);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					KeyReleasedEvent event(key);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_REPEAT:
+				{
+					KeyPressedEvent event(key, 1);
+					data.EventCallback(event);
+					break;	
+				}
+			}
+		});
+
+		glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			
+			switch (action)
+			{
+				case GLFW_PRESS:
+				{
+					MouseButtonPressedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					MouseButtonReleasedEvent event(button);
+					data.EventCallback(event);
+					break;
+				}
+			}
+		});
+
+		glfwSetScrollCallback(m_Window, [](GLFWwindow* window, double xOffset, double yOffset)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseScrolledEvent event((float)xOffset, (float)yOffset);
+			data.EventCallback(event);
+		});
+
+		glfwSetCursorPosCallback(m_Window, [](GLFWwindow* window, double xPos, double yPos)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseMovedEvent event((float)xPos, (float)yPos);
+			data.EventCallback(event);
+		});
+
 	}
 
 	void WindowsWindow::Shutdown()

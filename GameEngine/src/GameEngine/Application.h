68c57d3b9a2e19c74497a22aa5d4b8e7ba04ac92
iareb <lolluz6@gmail.com>
2025-02-23 18:12:34 +0100

    Aggiunto ImGui al progetto e prima implementazione del suo Layer

--- GameEngine/src/GameEngine/Application.h
+++ GameEngine/src/GameEngine/Application.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "Core.h"
-#include "Events/Event.h"
-#include "GameEngine/Events/ApplicationEvent.h"
+
 #include "Window.h"
+#include "LayerStack.h"
+#include "GameEngine/Events/Event.h"
+#include "GameEngine/Events/ApplicationEvent.h"
 
 namespace GameEngine {
 
@@ -17,10 +19,23 @@ namespace GameEngine {
 
 		void OnEvent(Event& e);
 
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* layer);
+
+		// Vogliamo richiamare il singleton di Application
+		// da qualsiasi punto del programma.
+		inline static Application& Get() { return *s_Instance; }
+
+		inline Window& GetWindow() { return *m_Window; }
+
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
+		LayerStack m_LayerStack;
+
+	private:
+		static Application* s_Instance;
 	};
 
 	// To be defined in CLIENT
--- premake5.lua
+++ premake5.lua
@@ -27,6 +27,7 @@ project "GameEngine"
     location "GameEngine"
     kind "SharedLib"
     language "C++"
+    staticruntime "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -61,7 +62,6 @@ project "GameEngine"
     -- I filtri sono utili per configurazioni che si applicano a una specifica piattaforma
     filter "system:windows"
         cppdialect "C++17"
-        staticruntime "On"
         systemversion "latest"
 
         defines 
@@ -79,17 +79,17 @@ project "GameEngine"
     
     filter "configurations:Debug"
         defines "HZ_DEBUG"
-        buildoptions "/MDd"
+        runtime "Debug"
         symbols "On"
     
     filter "configurations:Release"
         defines "HZ_RELEASE"
-        buildoptions "/MD"
+        runtime "Release"
         optimize "On"
 
     filter "configurations:Dist"
         defines "HZ_DIST"
-        buildoptions "/MD"
+        runtime "Release"
         optimize "On"
 
     filter "action:vs2022"
@@ -101,6 +101,7 @@ project "Sandbox"
     location "Sandbox"
     kind "ConsoleApp"
     language "C++"
+    staticruntime "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -124,7 +125,6 @@ project "Sandbox"
 
     filter "system:windows"
         cppdialect "C++17"
-        staticruntime "On"
         systemversion "latest"
 
         defines 
@@ -134,17 +134,17 @@ project "Sandbox"
 
     filter "configurations:Debug"
         defines "HZ_DEBUG"
-        buildoptions "/MDd"
+        runtime "Debug"
         symbols "On"
     
     filter "configurations:Release"
         defines "HZ_RELEASE"
-        buildoptions "/MD"
+        runtime "Release"
         optimize "On"
 
     filter "configurations:Dist"
         defines "HZ_DIST"
-        buildoptions "/MD"
+        runtime "Release"
         optimize "On"
 
     filter "action:vs2022"
